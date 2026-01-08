20d07b59ec513659d5e6c998b999fa6e7d20929d
KAUSTUBHOG <kaustubh.devlop@gmail.com>
2026-01-09 00:35:16 +0530

    added overall structire in lib.rs

--- programs/automatic_market_maker/src/lib.rs
+++ programs/automatic_market_maker/src/lib.rs
@@ -7,10 +7,16 @@ declare_id!("3ZrN6UmFKvZbkuEwJUBGV1RxbGgeXDzu2UPLj48PPmfk");
 pub mod automatic_market_maker {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_pool() -> Result<()> {
+        Ok(())
+    }
+    pub fn add_liquidity() -> Result<()> {
+        Ok(())
+    }
+    pub fn remove_liquidity() -> Result<()> {
+        Ok(())
+    }
+    pub fn swap() -> Result<()> {
         Ok(())
     }
 }
-
-
