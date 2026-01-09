f00e135cca6ed2cd44701dbaacc2fe64d17536a9
KAUSTUBHOG <kaustubh.devlop@gmail.com>
2026-01-09 23:41:44 +0530

    wrote the  oarams for a add_liquidity

--- programs/automatic_market_maker/src/instructions/add_liquidity.rs
+++ programs/automatic_market_maker/src/instructions/add_liquidity.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 #[derive(Accounts)]
 pub struct AddLiquidity {}
-pub fn handler() -> Result<()> {
+pub fn handler(ctx: Context<AddLiquidity>, amount_a: u64, amount_b: u64) -> Result<()> {
     Ok(())
 }
--- programs/automatic_market_maker/src/lib.rs
+++ programs/automatic_market_maker/src/lib.rs
@@ -14,21 +14,15 @@ pub mod automatic_market_maker {
         ctx: Context<InitializePool>,
         token_a: Pubkey,
         token_b: Pubkey,
-        
     ) -> Result<()> {
-        instructions::initialize_pool::handler(
-            ctx,
-            token_a,
-            token_b,
-            
-        )?;
+        instructions::initialize_pool::handler(ctx, token_a, token_b)?;
         Ok(())
     }
 
-    // pub fn add_liquidity(ctx: Context<AddLiquidity>) -> Result<()> {
-    //     instructions::add_liquidity::handler()?;
-    //     Ok(())
-    // }
+    pub fn add_liquidity(ctx: Context<AddLiquidity>, amount_a: u64, amount_b: u64) -> Result<()> {
+        instructions::add_liquidity::handler(ctx, amount_a, amount_b)?;
+        Ok(())
+    }
     // pub fn remove_liquidity(ctx: Context<RemoveLiquidity>) -> Result<()> {
     //     instructions::remove_liquidity::handler()?;
     //     Ok(())
